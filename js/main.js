50339bc79dec8e9ab1f7b2ee1d2024cd913402cc
akoobeEey <akmalmamurov99@gmail.com>
2023-07-23 16:50:07 +0500

    section start

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+const imgFull = document.querySelector(".akImg")
+
+const posTop = document.querySelector(".akTop")
+
+let myModal = new bootstrap.Modal(document.getElementById('myModal'), options)
+
